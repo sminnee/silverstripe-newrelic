bd0103c4d71bd913aecff7ff4c8057ee99382a81
Sam Minnee <sam@silverstripe.com>
2014-08-05 18:22:30 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+SilverStripe NewRelic Module
+============================
+
+This module adds better NewRelic metric collection to your SilverStripe site.
+
+To use
+
+ * Install NewRelic on your server
+ * Install this module: `composer require silverstripe/newrelic:*`
+
+
---
+++ code/NewRelicControllerExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+class NewRelicControllerExtension extends Extension {
+
+	function onAfterInit() {
+		if (extension_loaded('newrelic')) {
+			if($this->owner->getResponse()->isFinished()) {
+				$controller = get_class($this->owner);
+				newrelic_name_transaction("$controller/init");
+			}
+
+			if($memberID = Session::get('memberID')) {
+				newrelic_add_custom_parameter('memberID', $memberID);
+			}
+
+		}
+	}
+	function beforeCallActionHandler($request, $action) {
+		if (extension_loaded('newrelic')) {
+			$controller = get_class($this->owner);
+			newrelic_name_transaction("$controller/$action");
+		}
+	}
+}
+
+// Set-up calls
+if (extension_loaded('newrelic')) {
+	newrelic_add_custom_tracer("Director::direct");
+	newrelic_add_custom_tracer("SSViewer::process");
+}
