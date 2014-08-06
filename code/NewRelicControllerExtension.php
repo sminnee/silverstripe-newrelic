2ff4eb98fbf5e360b03bf3c787a3edef1bd66975
Sam Minnee <sam@silverstripe.com>
2014-08-06 14:20:35 +1200

    FIX: More robust transaction naming

--- code/NewRelicControllerExtension.php
+++ code/NewRelicControllerExtension.php
@@ -2,34 +2,48 @@
 
 class NewRelicControllerExtension extends Extension {
 
+	protected $transactionName = null;
+
 	function onAfterInit() {
-		if (extension_loaded('newrelic')) {
-			if($this->owner->getResponse()->isFinished()) {
-				$controller = get_class($this->owner);
-				newrelic_name_transaction("$controller/init");
-			}
+		$controller = get_class($this->owner);
 
-			if($memberID = Session::get('memberID')) {
-				newrelic_add_custom_parameter('memberID', $memberID);
-			}
+		if($this->owner->getResponse()->isFinished()) {
+			$this->transactionName = "$controller/init-only";
 
+		} else {
+			$action = $this->owner->getRequest()->latestParam('Action');
+			if(!$action) $action = 'index';
+			$this->transactionName = "$controller/$action";
 		}
 	}
+
 	function beforeCallActionHandler($request, $action) {
-		if (extension_loaded('newrelic')) {
-			$controller = get_class($this->owner);
-			newrelic_name_transaction("$controller/$action");
+		$controller = get_class($this->owner);
+		$this->transactionName = "$controller/$action";
+	}
+
+	function __destruct() {
+		if($this->transactionName) {
+			//Debug::message("newrelic_name_transaction($this->transactionName)");
+			if (extension_loaded('newrelic')) newrelic_name_transaction($this->transactionName);
+
+			if($memberID = Session::get('loggedInAs')) {
+				//Debug::message("newrelic_add_custom_parameter('memberID', $memberID)");
+				if(extension_loaded('newrelic')) newrelic_add_custom_parameter('memberID', $memberID);
+			}
 		}
 	}
 }
 
 // Set-up calls
 if (extension_loaded('newrelic')) {
-	newrelic_add_custom_tracer("Director::direct,SSViewer::process");
+	//newrelic_add_custom_tracer("Director::direct,SSViewer::process");
 
-	// Set the name to the vhost
-	// To do: make this the canonical vhost, defined by $_FILE_TO_URL_MAPPING
-	if(isset($_SERVER['HTTP_HOST'])) newrelic_set_appname($_SERVER['HTTP_HOST']);
+	// Set the name to the vhost for client-side scripts
+	// To do: get robust way of picking the canoncial hostname for www requests too
+	if(Director::is_cli() && isset($_SERVER['HTTP_HOST'])) {
+		newrelic_set_appname($_SERVER['HTTP_HOST']);
+	}
 
 	// Distinguish background from web requests more reliably
 	newrelic_background_job(Director::is_cli());
