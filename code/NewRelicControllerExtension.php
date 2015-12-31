8b6e7ce64df2c6fc9102aa0cf40a7085119d5be9
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:40:31 +1300

    Converted to PSR-2

--- code/NewRelicControllerExtension.php
+++ code/NewRelicControllerExtension.php
@@ -1,51 +1,59 @@
 <?php
 
-class NewRelicControllerExtension extends Extension {
+class NewRelicControllerExtension extends Extension
+{
 
-	protected $transactionName = null;
+    protected $transactionName = null;
 
-	function onAfterInit() {
-		$controller = get_class($this->owner);
+    public function onAfterInit()
+    {
+        $controller = get_class($this->owner);
 
-		if($this->owner->getResponse()->isFinished()) {
-			$this->transactionName = "$controller/init-only";
+        if ($this->owner->getResponse()->isFinished()) {
+            $this->transactionName = "$controller/init-only";
+        } else {
+            $action = $this->owner->getRequest()->latestParam('Action');
+            if (!$action) {
+                $action = 'index';
+            }
+            $this->transactionName = "$controller/$action";
+        }
+    }
 
-		} else {
-			$action = $this->owner->getRequest()->latestParam('Action');
-			if(!$action) $action = 'index';
-			$this->transactionName = "$controller/$action";
-		}
-	}
+    public function beforeCallActionHandler($request, $action)
+    {
+        $controller = get_class($this->owner);
+        $this->transactionName = "$controller/$action";
+    }
 
-	function beforeCallActionHandler($request, $action) {
-		$controller = get_class($this->owner);
-		$this->transactionName = "$controller/$action";
-	}
+    public function __destruct()
+    {
+        if ($this->transactionName) {
+            //Debug::message("newrelic_name_transaction($this->transactionName)");
+            if (extension_loaded('newrelic')) {
+                newrelic_name_transaction($this->transactionName);
+            }
 
-	function __destruct() {
-		if($this->transactionName) {
-			//Debug::message("newrelic_name_transaction($this->transactionName)");
-			if (extension_loaded('newrelic')) newrelic_name_transaction($this->transactionName);
-
-			if($memberID = Session::get('loggedInAs')) {
-				//Debug::message("newrelic_add_custom_parameter('memberID', $memberID)");
-				if(extension_loaded('newrelic')) newrelic_add_custom_parameter('memberID', $memberID);
-			}
-		}
-	}
+            if ($memberID = Session::get('loggedInAs')) {
+                //Debug::message("newrelic_add_custom_parameter('memberID', $memberID)");
+                if (extension_loaded('newrelic')) {
+                    newrelic_add_custom_parameter('memberID', $memberID);
+                }
+            }
+        }
+    }
 }
 
 // Set-up calls
 if (extension_loaded('newrelic')) {
-	//newrelic_add_custom_tracer("Director::direct,SSViewer::process");
+    //newrelic_add_custom_tracer("Director::direct,SSViewer::process");
 
-	// Set the name to the vhost for client-side scripts
-	// To do: get robust way of picking the canoncial hostname for www requests too
-	if(Director::is_cli() && isset($_SERVER['HTTP_HOST'])) {
-		newrelic_set_appname($_SERVER['HTTP_HOST']);
-	}
+    // Set the name to the vhost for client-side scripts
+    // To do: get robust way of picking the canoncial hostname for www requests too
+    if (Director::is_cli() && isset($_SERVER['HTTP_HOST'])) {
+        newrelic_set_appname($_SERVER['HTTP_HOST']);
+    }
 
-	// Distinguish background from web requests more reliably
-	newrelic_background_job(Director::is_cli());
-
+    // Distinguish background from web requests more reliably
+    newrelic_background_job(Director::is_cli());
 }
